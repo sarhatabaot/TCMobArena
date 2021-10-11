166744bcbaaa40ca1ecb38b22f94ecbc53ee87d5
Omer Oreg <sarhatabaot1@gmail.com>
2021-10-11 19:46:18 +0300

    Updated to use latest api

--- src/main/java/com/github/sarhatabaot/tcmobarena/MobArenaListener.java
+++ src/main/java/com/github/sarhatabaot/tcmobarena/MobArenaListener.java
@@ -1,12 +1,11 @@
 package com.github.sarhatabaot.tcmobarena;
 
-import com.garbagemule.MobArena.MobArena;
-
 import net.tinetwork.tradingcards.api.TradingCardsPlugin;
 import net.tinetwork.tradingcards.api.addons.AddonListener;
 import net.tinetwork.tradingcards.api.addons.TradingCardsAddon;
-import net.tinetwork.tradingcards.api.blacklist.Blacklist;
-import org.bukkit.Bukkit;
+import net.tinetwork.tradingcards.api.card.Card;
+import net.tinetwork.tradingcards.api.model.DropType;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -16,19 +15,15 @@ import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MobArenaListener extends AddonListener {
-    private final TradingCardsPlugin tradingCards;
-    private TCMobArena addonPlugin;
+    private final TradingCardsPlugin<? extends Card<? extends Card>> tradingCards;
     private CommentedConfigurationNode configurationNode;
 
-    public MobArenaListener(final TradingCardsAddon tradingCardsAddon, final TradingCardsPlugin tradingCards, final TCMobArena addonPlugin) {
+    public MobArenaListener(final TradingCardsAddon tradingCardsAddon, final TradingCardsPlugin tradingCards) {
         super(tradingCardsAddon);
         this.tradingCards = tradingCards;
-        this.addonPlugin = addonPlugin;
         try {
             this.configurationNode = YamlConfigurationLoader.builder()
                     .path(Path.of("config.yml"))
@@ -38,48 +33,40 @@ public class MobArenaListener extends AddonListener {
         }
     }
 
-	/*public MobArenaListener(final TradingCards tradingCards, final JavaPlugin plugin) {
-		this.tradingCards = tradingCards;
-		this.plugin = plugin;
-	}*/
-
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
-        MobArena maPlugin = (MobArena) Bukkit.getServer().getPluginManager().getPlugin("MobArena");
-        if (maPlugin == null || !maPlugin.isEnabled() || !configurationNode.node("disable-in-arena").getBoolean()) {
+        if (!configurationNode.node("disable-in-arena").getBoolean()) {
             return;
         }
 
-        boolean canDropPlayer = false;
-        boolean canDropWorld = false;
-
-        if (event.getEntity().getKiller() != null) {
-            Player p = event.getEntity().getKiller();
-            canDropPlayer = tradingCards.getPlayerBlacklist().isAllowed(p); //TODO
-            canDropWorld = tradingCards.getWorldBlacklist().isAllowed(p.getWorld().getName());
-        }
-        if (!canDropPlayer || !canDropWorld) {
+        if(event.getEntity().getKiller() == null) {
             return;
         }
 
-        String rare = tradingCards.getCardManager().getRandomRarity(event.getEntityType(), false);
-        var cancelled = false;
-        if ("None".equalsIgnoreCase(rare))
+        final Player player = event.getEntity().getKiller();
+        if (!canDropPlayer(player) || !canDropWorld(player.getWorld())) {
+            return;
+        }
+
+        DropType dropType = tradingCards.getDropTypeManager().getMobType(event.getEntityType());
+        String rare = tradingCards.getCardManager().getRandomRarity(dropType, false);
+        if ("none".equalsIgnoreCase(rare))
             return;
 
-        if (tradingCards.getConfig().getBoolean("General.Spawner-Block") && event.getEntity().getCustomName() != null && event.getEntity().getCustomName().equals(tradingCards.getConfig().getString("General.Spawner-Mob-Name"))) {
-            tradingCards.debug("Mob came from spawner, not dropping card.");
-            cancelled = true;
-        }
-
-        if (!cancelled) {
-            event.getDrops().add(getRandomDrop(rare));
-        }
+        event.getDrops().add(getRandomDrop(rare));
     }
 
     private ItemStack getRandomDrop(final String rarity) {
         if (configurationNode.node("use-active-series").getBoolean())
             return tradingCards.getCardManager().getRandomActiveCard(rarity, false).build();
         return tradingCards.getCardManager().getRandomCard(rarity, false).build();
+    }
+
+    private boolean canDropPlayer(Player player) {
+        return tradingCards.getPlayerBlacklist().isAllowed(player);
+    }
+
+    private boolean canDropWorld(World world) {
+        return tradingCards.getWorldBlacklist().isAllowed(world);
     }
 }
--- src/main/java/com/github/sarhatabaot/tcmobarena/TCMobArena.java
+++ src/main/java/com/github/sarhatabaot/tcmobarena/TCMobArena.java
@@ -4,7 +4,9 @@ package com.github.sarhatabaot.tcmobarena;
 import net.tinetwork.tradingcards.api.TradingCardsPlugin;
 import net.tinetwork.tradingcards.api.addons.AddonLogger;
 import net.tinetwork.tradingcards.api.addons.TradingCardsAddon;
+import net.tinetwork.tradingcards.api.card.Card;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class TCMobArena extends JavaPlugin implements TradingCardsAddon {
 	private AddonLogger addonLogger;
@@ -12,18 +14,24 @@ public final class TCMobArena extends JavaPlugin implements TradingCardsAddon {
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
-		TradingCardsPlugin tradingCards = (TradingCardsPlugin) getServer().getPluginManager().getPlugin("TradingCards");
+		TradingCardsPlugin<? extends Card<? extends Card>> tradingCards = (TradingCardsPlugin<? extends Card<? extends Card>>) getServer().getPluginManager().getPlugin("TradingCards");
 		addonLogger = new AddonLogger(getName(), tradingCards);
-		getServer().getPluginManager().registerEvents(new MobArenaListener(this,tradingCards, this),this);
+		getServer().getPluginManager().registerEvents(new MobArenaListener(this,tradingCards),this);
 	}
 
 	@Override
-	public JavaPlugin getJavaPlugin() {
+	public @NotNull JavaPlugin getJavaPlugin() {
 		return this;
 	}
 
 	@Override
 	public AddonLogger getAddonLogger() {
 		return addonLogger;
+	}
+
+	@Override
+	public void onReload() {
+		onDisable();
+		onEnable();
 	}
 }
